8e504ad0fbe082fbe59802c34e348d2d91680bc3
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-21 16:32:42 +0800

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,1 +1,1 @@
-module.exports=(q,s)=>{s.status(200).json({code:0,data:{token:btoa('b3b46a4146e79d57c5d3227cdf949f0e')}});};
+module.exports=(q,s)=>{s.status(200).json({code:0,data:{token:'YjNiNDZhNDE0NmU3OWQ1N2M1ZDMyMjdjZGY5NDlmMGU='}});};
