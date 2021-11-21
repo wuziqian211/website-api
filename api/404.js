d475c3edcca7d4d066e1e4022f66effdb71d3cdc
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-21 20:11:57 +0800

    Update 404.js

--- api/404.js
+++ api/404.js
@@ -6,6 +6,6 @@ module.exports=(q,s)=>{s.status(404).send(`<!DOCTYPE html>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   </head>
   <body>
-    <div id="404"><div style="font-size: 40px; font-weight: bold;">wuziqian211's Blog - 您似乎输入错了网址！</div>本网站不是您要访问的网站，您是否想要访问 <a href="https://wuziqian211.top${q.url}" id="URL">https://wuziqian211.top${q.url}</a>？</div>
+    <div><div style="font-size: 40px; font-weight: bold;">wuziqian211's Blog - 您似乎输入错了网址！</div>本网站不是您要访问的网站，您是否想要访问 <a href="https://wuziqian211.top${q.url}">https://wuziqian211.top${q.url}</a>？</div>
   </body>
 </html>`);};
