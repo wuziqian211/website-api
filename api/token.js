65a3826eb7c9eb96d9469aaf3fb768504739d639
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-23 23:13:29 +0800

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,1 +1,8 @@
-module.exports=(q,s)=>{s.status(200).json({code:0,data:{token:'YjNiNDZhNDE0NmU3OWQ1N2M1ZDMyMjdjZGY5NDlmMGU='}});};
+module.exports = (req, res) => {
+  res.status(200).json({
+    code: 0,
+    data: {
+      token: 'YjNiNDZhNDE0NmU3OWQ1N2M1ZDMyMjdjZGY5NDlmMGU='
+    }
+  });
+};
