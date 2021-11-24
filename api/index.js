fa3166927abd51a3e06ee6e8b836ac28588e3ff2
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-24 23:12:10 +0800

    Update and rename test.js to index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
 module.exports = (req, res) => {
   if (req.headers.accept.split(',').indexOf('text/html') == -1) {
-    res.status(200).json({code: 0});
+    res.status(404).json({code: -404});
   } else {
     res.status(200).send(`<!DOCTYPE html>
 <html lang="zh-CN">
   <head>
     <meta charset="utf-8">
-    <title>欢迎 | wuziqian211's Blog</title>
+    <title>欢迎来到 API 页面 | wuziqian211's Blog</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   </head>
   <body>
-    <div><div style="font-size: 40px; font-weight: bold;">wuziqian211's Blog - 欢迎您来到API页面！</div>本网站主要用于执行API操作。</div>
+    <div><div style="font-size: 40px; font-weight: bold;">wuziqian211's Blog - 欢迎您来到 API 页面！</div>本网站主要为 wuziqian211's Blog 的一些功能提供服务。</div>
   </body>
 </html>`);
   }
