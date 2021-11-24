5c407825b93c024dffe7fbe8ab1d4d527d49003d
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-24 23:04:05 +0800

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  if (req.headers.accept.split(',').indexOf('text/html') == -1 && req.headers.accept != '*/*') {
+  if (req.headers.accept.split(',').indexOf('text/html') == -1) {
     res.status(200).json({code: 0});
   } else {
     res.status(200).send(`<!DOCTYPE html>
@@ -10,7 +10,7 @@ module.exports = (req, res) => {
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   </head>
   <body>
-    <div><div style="font-size: 40px; font-weight: bold;">wuziqian211's Blog - 欢迎您来到API页面！</div>本网站主要是用来执行API操作的。</div>
+    <div><div style="font-size: 40px; font-weight: bold;">wuziqian211's Blog - 欢迎您来到API页面！</div>本网站主要用于执行API操作。</div>
   </body>
 </html>`);
   }
