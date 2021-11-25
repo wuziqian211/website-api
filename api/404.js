4610847b323407663b999c769cd8fe59857b033c
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-25 21:07:26 +0800

    Update 404.js

--- api/404.js
+++ api/404.js
@@ -1,3 +1,17 @@
 module.exports = (req, res) => {
-  res.status(404).json({code: -404});
+  if (req.headers.accept.split(',').indexOf('text/html') == -1) {
+    res.status(404).json({code: -404});
+  } else {
+    res.status(404).send(`<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <title>页面不存在 | wuziqian211's Blog</title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+    <div><div style="font-size: 40px; font-weight: bold;">wuziqian211's Blog - API 不存在</div>您访问的 API 不存在，请到<a href="https://api.wuziqian211.top/">首页</a>查看目前可用的 API 列表</div>
+  </body>
+</html>`);
+  }
 };
