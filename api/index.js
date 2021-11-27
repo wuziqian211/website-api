b278dd1a8e0c2a9a674b19a2acd38d1ed4a9716c
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-27 23:43:08 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  if (req.headers.accept.split(',').indexOf('text/html') == -1) {
+  if (!req.headers.accept || req.headers.accept.split(',').indexOf('text/html') == -1) {
     res.status(404).json({code: -404});
   } else {
     res.status(200).send(`<!DOCTYPE html>
