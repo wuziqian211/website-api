592d1b882e384728de71c697c865a9c63a458135
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-12-04 19:34:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,43 @@ module.exports = (req, res) => {
   if (!req.headers.accept || req.headers.accept.split(',').indexOf('text/html') == -1) {
     res.status(404).json({code: -404});
   } else {
-    res.status(200).send(`<!DOCTYPE html>
+    res.status(300).send(`<!DOCTYPE html>
 <html lang="zh-CN">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>欢迎来到 API 页面 | wuziqian211's Blog</title>
+    <style type="text/css">body {
+  background-color: #0078B7;
+  color: #FFF;
+  font-family: "Segoe UI", "Arial", "Microsoft Yahei", Helvetica, sans-serif;
+  padding: 90px;
+}
+.face {
+  font-size: 100px;
+}
+.tips {
+  display: inline-block;
+  padding-top: 10px;
+  font-size: 14px;
+  line-height: 20px;
+}
+a {
+  color: #FFF;
+}
+p {
+  font-size: 24px;
+}</style>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   </head>
   <body>
-    <div><div style="font-size: 40px; font-weight: bold;">wuziqian211's Blog - 欢迎您来到 API 页面！</div>本网站主要为 wuziqian211's Blog 的一些功能提供服务。</div>
+    <!-- Reference code: status.fastgit.org -->
+    <span class="face">:)</span>
+    <p>欢迎您来到 API 页面！</p>
+    <p>本网站主要为 wuziqian211's Blog 的一些功能提供服务。</p>
+    <p>可用API：</p>
+    <p><a href="/getbili.js">获取哔哩哔哩用户信息</a></p>
+    <span class="tips">MULTIPLE_CHOICES</span>
   </body>
 </html>`);
   }
