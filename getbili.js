943419161726ffb7959483e4db1347b35b49bc79
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-12-05 00:13:54 +0800

    Update getbili.js

--- getbili.js
+++ getbili.js
@@ -46,24 +46,26 @@ img {
     const fetch = require('node-fetch');
     fetch(`https://api.bilibili.com/x/space/acc/info?mid=${req.query.mid}`).then(resp => resp.json()).then(json => {
       if (req.headers.accept && req.headers.accept.indexOf('html') != -1) {
-        if (json.code == 0)
+        if (json.code == 0) {
           sendHTML({code: 200, title: `${json.data.name} 的用户信息`, face: ')', content: `昵称：${json.data.name}<br>头像：<br><img alt="${json.data.name}" src="/getbili.js?mid=${req.query.mid}">`, tips: 'OK'});
-        else if (json.code == -412)
+        } else if (json.code == -412) {
           sendHTML({code: 412, title: '操作太频繁', face: '(', content: '您的请求过于频繁，已被 B 站拦截qwq<br>请稍后重试awa', tips: 'PRECONDITION_FAILED'});
-        else if (json.code == -404)
+        } else if (json.code == -404) {
           sendHTML({code: 404, title: '用户不存在', face: '(', content: `UID${req.query.mid} 对应的用户不存在！QAQ`, tips: 'NOT_FOUND'});
-        else
+        } else {
           sendHTML({code: 400, title: '获取用户信息失败', face: '(', content: `获取 UID${req.query.mid} 的信息失败，请稍后重试awa`, tips: 'BAD_REQUEST'});
+        }
       } else if (req.headers.accept && req.headers.accept.indexOf('image') != -1) {
         var header;
-        if (json.code == 0)
+        if (json.code == 0) {
           fetch(json.data.face).then(img => {
             header = img.headers.get('Content-Type');
             return img.buffer();
           }).then(buffer => res.setHeader('Content-Type', header).status(200).send(buffer));
-        else
+        } else {
           fetch('http://i0.hdslb.com/bfs/face/member/noface.jpg').then(img => img.buffer()).then(buffer => res.setHeader('Content-Type', 'image/jpg').status(404).send(buffer));
-      } else if (json.code == 0)
+        }
+      } else if (json.code == 0) {
         res.status(200).json({
           code: 0,
           data: {
@@ -71,18 +73,21 @@ img {
             face: json.data.face
           }
         });
-      else if (json.code == -412)
+      } else if (json.code == -412) {
         res.status(412).json({code: -412});
-      else if (json.code == -404)
+      } else if (json.code == -404) {
         res.status(404).json({code: -404});
-      else
+      } else {
         res.status(400).json({code: json.code});
+      }
     });
   } else if (req.headers.accept && req.headers.accept.indexOf('html') != -1) {
-    if (req.query.mid == undefined)
-      sendHTML({code: 200, title: '获取哔哩哔哩用户信息', face: ')', content: '您输入的 UID 无效！<br>请输入一个正确的 UID 吧awa', tips: 'BAD_REQUEST'})
-    else
-      sendHTML({code: 400, title: 'UID 无效', face: '(', content: '您输入的 UID 无效！<br>请输入一个正确的 UID 吧awa', tips: 'BAD_REQUEST'})
-  } else
+    if (req.query.mid == undefined) {
+      sendHTML({code: 200, title: '获取哔哩哔哩用户信息', face: ')', content: '您输入的 UID 无效！<br>请输入一个正确的 UID 吧awa', tips: 'BAD_REQUEST'});
+    } else {
+      sendHTML({code: 400, title: 'UID 无效', face: '(', content: '您输入的 UID 无效！<br>请输入一个正确的 UID 吧awa', tips: 'BAD_REQUEST'});
+    }
+  } else {
     res.status(400).json({code: -400});
+  }
 };
