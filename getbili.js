ac777c283d11b67c769e51c99f225966bdde1e58
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-12-04 23:07:10 +0800

    Update getbili.js

--- getbili.js
+++ getbili.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   if (/^[0-9]+$/.test(req.query.mid)) {
     const fetch = require('node-fetch');
     fetch(`https://api.bilibili.com/x/space/acc/info?mid=${req.query.mid}`).then(resp => resp.json()).then(json => {
-      if (req.query.type == '1') {
+      if (req.headers.accept && req.headers.accept.indexOf('image') != -1) {
         var header;
         if (json.code == 0) {
           fetch(json.data.face).then(img => {
@@ -12,6 +12,52 @@ module.exports = (req, res) => {
         } else {
           fetch('http://i0.hdslb.com/bfs/face/member/noface.jpg').then(img => img.buffer()).then(buffer => res.setHeader('Content-Type', 'image/jpg').status(404).send(buffer));
         }
+      } else if (req.headers.accept && req.headers.accept.indexOf('html') != -1) {
+        if (json.code == 0) {
+          var d = {code: 200, title: `${json.data.name} 的用户信息`, face: ')', content: `昵称：${json.data.name}<br>头像：<img alt="${json.data.name}" src="/getbili.js?mid=${req.query.mid}">`, tips: 'OK'};
+        } else if (json.code == -412) {
+          var d = {code: 412, title: '操作太频繁', face: '(', content: '您的请求过于频繁，已被 B 站拦截qwq<br>请稍后重试awa', tips: 'PRECONDITION_FAILED'};
+        } else if (json.code == -404) {
+          var d = {code: 404, title: '用户不存在', face: '(', content: `UID${req.query.mid} 对应的用户不存在！QAQ`, tips: 'NOT_FOUND'};
+        } else {
+          var d = {code: 400, title: '获取用户信息失败', face: '(', content: `获取 UID${req.query.mid} 的信息失败，请稍后重试awa`, tips: 'BAD_REQUEST'};
+        }
+        res.status(d.code).send(`<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>${d.title} | wuziqian211's Blog</title>
+    <style type="text/css">body {
+  background-color: #0078B7;
+  color: #FFF;
+  font-family: "Segoe UI", "Arial", "Microsoft Yahei", Helvetica, sans-serif;
+  padding: 90px;
+}
+.face {
+  font-size: 100px;
+}
+.tips {
+  display: inline-block;
+  padding-top: 10px;
+  font-size: 14px;
+  line-height: 20px;
+}
+a {
+  color: #FFF;
+}
+p {
+  font-size: 24px;
+}</style>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+    <!-- Reference code: status.fastgit.org -->
+    <span class="face">:${d.face}</span>
+    <p>${d.content}</p>
+    <span class="tips">${d.tips}</span>
+  </body>
+</html>`);
       } else if (json.code == 0) {
         res.status(200).json({
           code: 0,
@@ -20,11 +66,13 @@ module.exports = (req, res) => {
             face: json.data.face
           }
         });
-      } else {
+      } else if (json.code == -412) {
+        res.status(412).json({code: -412});
+      } else if (json.code == -404) {
         res.status(404).json({code: -404});
+      } else {
+        res.status(400).json({code: json.code});
       }
     });
-  } else {
-    res.status(400).json({code: -400});
   }
 };
