8c02f47eb3357dd4951371968024b0caf630838d
wuziqian211 <3379226102@qq.com>
2021-12-19 21:02:50 +0800

    update

--- get.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  const fetch = require('node-fetch');
-  var header;
-  fetch(req.query.url).then(resp => {
-    header = resp.headers.get('Content-Type');
-    return resp.buffer();
-  }).then(resp => res.setHeader('Content-Type', header).status(200).send(resp));
-};
