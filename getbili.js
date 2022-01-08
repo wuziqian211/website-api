a6c14230430e42efa02e83567ec8a9833fd1257e
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-01-08 11:36:59 +0800

    Update getbili.js

--- getbili.js
+++ getbili.js
@@ -15,8 +15,8 @@ module.exports = (req, res) => {
     <p class="content">${data.content}</p>
     <form action="/getbili.js" method="GET">
       <label for="mid">请输入要获取用户信息的 UID：</label>
-      <input type="number" name="mid" id="mid" value="${data.mid}" autocomplete="off">
-      <input type="submit" value="获取">
+      <input type="number" name="mid" id="mid" value="${data.mid}" autocomplete="off" />
+      <input type="submit" value="获取" />
     </form>
     <p class="home"><a href="/">返回 API 首页</a></p>
     <span class="tips">${data.tips}</span>
@@ -28,9 +28,9 @@ module.exports = (req, res) => {
     fetch(`https://api.bilibili.com/x/space/acc/info?mid=${req.query.mid}`).then(resp => resp.json()).then(json => {
       if (req.headers.accept && req.headers.accept.indexOf('html') != -1) {
         if (json.code == 0) {
-          sendHTML({code: 200, title: `${json.data.name} 的用户信息`, face: ')', content: `昵称：${json.data.name}<br>头像：<br><img alt="${json.data.name}" src="/getbili.js?mid=${req.query.mid}">`, mid: req.query.mid, tips: 'OK'});
+          sendHTML({code: 200, title: `${json.data.name} 的用户信息`, face: ')', content: `昵称：${json.data.name}<br />头像：<br /><img alt="${json.data.name}" src="/getbili.js?mid=${req.query.mid}" />`, mid: req.query.mid, tips: 'OK'});
         } else if (json.code == -412) {
-          sendHTML({code: 412, title: '操作太频繁', face: '(', content: '您的请求过于频繁，已被 B 站拦截qwq<br>请稍后重试awa', mid: req.query.mid, tips: 'PRECONDITION_FAILED'});
+          sendHTML({code: 412, title: '操作太频繁', face: '(', content: '您的请求过于频繁，已被 B 站拦截qwq<br />请稍后重试awa', mid: req.query.mid, tips: 'PRECONDITION_FAILED'});
         } else if (json.code == -404) {
           sendHTML({code: 404, title: '用户不存在', face: '(', content: `UID${req.query.mid} 对应的用户不存在！QAQ`, mid: req.query.mid, tips: 'NOT_FOUND'});
         } else {
@@ -66,9 +66,9 @@ module.exports = (req, res) => {
     });
   } else if (req.headers.accept && req.headers.accept.indexOf('html') != -1) {
     if (!req.query.mid || req.query.mid == '') {
-      sendHTML({code: 200, title: '获取哔哩哔哩用户信息', face: ')', content: `本 API 可以获取指定 B 站用户的信息。<br>用法：https://api.wuziqian211.top/getbili.js?mid={您想获取信息的 UID}`, mid: '', tips: 'OK'});
+      sendHTML({code: 200, title: '获取哔哩哔哩用户信息', face: ')', content: `本 API 可以获取指定 B 站用户的信息。<br />用法：https://api.wuziqian211.top/getbili.js?mid={您想获取信息的 UID}`, mid: '', tips: 'OK'});
     } else {
-      sendHTML({code: 400, title: 'UID 无效', face: '(', content: '您输入的 UID 无效！<br>请输入一个正确的 UID 吧awa', mid: '', tips: 'BAD_REQUEST'});
+      sendHTML({code: 400, title: 'UID 无效', face: '(', content: '您输入的 UID 无效！<br />请输入一个正确的 UID 吧awa', mid: '', tips: 'BAD_REQUEST'});
     }
   } else {
     res.status(400).json({code: -400});
