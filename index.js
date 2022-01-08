8ab28ccd53a4955ad4279dce5d7412d9da85650a
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-01-08 11:45:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = (req, res) => {
     <!-- Reference code: status.fastgit.org -->
     <span class="face">:)</span>
     <p class="content">欢迎您来到 API 页面！<br />这些 API 主要为 wuziqian211's Blog 的一些功能提供服务。</p>
-    <p class="content">可用 API：<br /><a href="/getbili.js">获取哔哩哔哩指定用户信息</a><a href="/getfollow.js">获取哔哩哔哩指定用户关注、粉丝数</a></p>
+    <p class="content">可用 API：<br /><a href="/getbili.js">获取哔哩哔哩指定用户信息</a><br /><a href="/getfollow.js">获取哔哩哔哩指定用户关注、粉丝数</a></p>
     <p class="home"><a href="https://wuziqian211.top/">返回网站首页</a></p>
     <span class="tips">MULTIPLE_CHOICES</span>
   </body>
