7275eea3034f9768299047b2c09199a97cfe791d
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-01-09 14:10:24 +0800

    Update 404.js

--- 404.js
+++ 404.js
@@ -5,11 +5,11 @@ module.exports = (req, res) => {
     res.status(404).send(`<!DOCTYPE html>
 <html lang="zh-CN">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>API 不存在 | wuziqian211's Blog API</title>
-    <link rel="stylesheet" href="/style.css">
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/style.css" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
   </head>
   <body>
     <!-- Reference code: status.fastgit.org -->
