a3d5aae153233522d93a97690b9ea910eb837066
wuziqian211 <3379226102@qq.com>
2022-01-15 23:42:15 +0800

    fix

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ module.exports = (req, res) => {
     <title>欢迎来到 API 页面 | wuziqian211's Blog API</title>
     <link rel="stylesheet" href="/style.css" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="/animate.min.css" />    <script src="/pjax.min.js"></script>
+    <link rel="stylesheet" href="/animate.min.css" />
+    <script src="/pjax.min.js"></script>
   </head>
   <body>
     <!-- Reference code: status.fastgit.org -->
