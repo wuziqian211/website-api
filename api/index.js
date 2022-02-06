db978c30eae84458578636cb38f63ce6dbb64747
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-02-06 14:06:41 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
     res.status(200).send(HTML({title: '欢迎来到 API 页面', data: `
       <span class="face animate__animated animate__fadeIn animate__faster">:)</span>
       <p class="content animate__animated animate__fadeIn animate__faster">欢迎您来到 API 页面！<br />这些 API 主要为 wuziqian211's Blog 的一些功能提供服务。</p>
-      <p class="content animate__animated animate__fadeIn animate__faster">可用 API：<br /><a href="/getbili.js">获取哔哩哔哩指定用户信息及关注、粉丝数</a></p>
+      <p class="content animate__animated animate__fadeIn animate__faster">可用 API：<br /><a href="/api/getbili">获取哔哩哔哩指定用户信息及关注、粉丝数</a></p>
       <p class="home animate__animated animate__fadeIn animate__faster"><a href="https://wuziqian211.top/">返回网站首页</a></p>
       <span class="tips animate__animated animate__fadeIn animate__faster">MULTIPLE_CHOICES</span>`}));
   }
