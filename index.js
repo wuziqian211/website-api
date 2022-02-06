2cb535081e76461c4d7c8e70c4066a83612eb010
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-02-06 12:55:10 +0800

    update

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
+const HTML = require('./html');
 module.exports = (req, res) => {
   if ((!req.headers.accept || req.headers.accept.indexOf('html') === -1) && req.headers['x-pjax'] !== 'true') {
     res.status(404).json({code: -404});
   } else {
-    res.status(200).send(`<!DOCTYPE html>
-<html lang="zh-CN">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#0078B7" media="(prefers-color-scheme: light)" />
-    <meta name="theme-color" content="#000064" media="(prefers-color-scheme: dark)" />
-    <title>欢迎来到 API 页面 | wuziqian211's Blog API</title>
-    <link rel="stylesheet" href="/assets/style.css" />
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="/assets/animate.min.css" />
-    <script src="/assets/pjax.min.js"></script>
-  </head>
-  <body>
-    <!-- Reference code: status.fastgit.org -->
-    <div class="data-pjax">
+    res.status(200).send(HTML({title: '欢迎来到 API 页面', data: `
       <span class="face animate__animated animate__fadeIn animate__faster">:)</span>
       <p class="content animate__animated animate__fadeIn animate__faster">欢迎您来到 API 页面！<br />这些 API 主要为 wuziqian211's Blog 的一些功能提供服务。</p>
       <p class="content animate__animated animate__fadeIn animate__faster">可用 API：<br /><a href="/getbili.js">获取哔哩哔哩指定用户信息及关注、粉丝数</a></p>
       <p class="home animate__animated animate__fadeIn animate__faster"><a href="https://wuziqian211.top/">返回网站首页</a></p>
-      <span class="tips animate__animated animate__fadeIn animate__faster">MULTIPLE_CHOICES</span>
-    </div>
-    <script>
-      const pjax = new Pjax({selectors: ['title', '.data-pjax'], cacheBust: false});
-      document.addEventListener('pjax:send', () => document.querySelectorAll('.animate__animated').forEach(e => {
-        e.classList.remove('animate__fadeIn');
-        e.classList.add('animate__fadeOut');
-      }));
-      document.addEventListener('pjax:error', () => document.location.href = event.request.responseURL);
-    </script>
-  </body>
-</html>`);
+      <span class="tips animate__animated animate__fadeIn animate__faster">MULTIPLE_CHOICES</span>`}));
   }
 };
--- modules.js
+++ modules.js
@@ -1,3 +1,4 @@
+const HTML = require('./html');
 module.exports = (req, res) => {
   if ((!req.headers.accept || req.headers.accept.indexOf('html') === -1) && req.headers['x-pjax'] !== 'true') {
     switch (req.query.id) {
@@ -5,16 +6,16 @@ module.exports = (req, res) => {
         res.status(200).json({code: 0, data: {token: 'YjNiNDZhNDE0NmU3OWQ1N2M1ZDMyMjdjZGY5NDlmMGU='}});
         break;
       case 'thanks':
-        let HTML = '';
+        let html = '';
         [
           {image: '/images/bilibili.svg', title: '哔哩哔哩', content: '提供 wuziqian211 发布视频、动态，与粉丝互动等的地方。哔哩哔哩是中国年轻世代高度聚集的综合性视频社区，被用户亲切地称为“B 站”。', link: 'https://www.bilibili.com/'},
           {image: '/images/you.png', title: '您', content: '支持 wuziqian211。自从您关注 wuziqian211 以来，虽然 TA 可能会犯各种各样的错误，您也一直在支持着 TA。', link: 'https://space.bilibili.com/'}
-        ].sort(() => 0.5 - Math.random()).forEach(i => HTML += `<div class="link-grid-container">
+        ].sort(() => 0.5 - Math.random()).forEach(i => html += `<div class="link-grid-container">
 <object class="link-grid-image" data="${i.image}"></object>
 <p>${i.title}</p><p>${i.content}</p>
 <a target="_blank" rel="noopener external nofollow noreferrer" href="${i.link}"></a>
 </div>`);
-        res.status(200).json({code: 0, data: HTML});
+        res.status(200).json({code: 0, data: html});
         break;
       case 'update':
         if (parseInt(req.query.version) > 0) {
@@ -37,36 +38,10 @@ module.exports = (req, res) => {
         res.status(400).json({code: -400});
     }
   } else {
-    res.status(404).send(`<!DOCTYPE html>
-<html lang="zh-CN">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#0078B7" media="(prefers-color-scheme: light)" />
-    <meta name="theme-color" content="#000064" media="(prefers-color-scheme: dark)" />
-    <title>API 不存在 | wuziqian211's Blog API</title>
-    <link rel="stylesheet" href="/assets/style.css" />
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="/assets/animate.min.css" />
-    <script src="/assets/pjax.min.js"></script>
-  </head>
-  <body>
-    <!-- Reference code: status.fastgit.org -->
-    <div class="data-pjax">
+    res.status(404).send(HTML({title: 'API 不存在', data: `
       <span class="face animate__animated animate__fadeIn animate__faster">:(</span>
       <p class="content animate__animated animate__fadeIn animate__faster">您访问的 API 不存在，请到<a href="/">首页</a>查看目前可用的 API 列表</p>
       <p class="home animate__animated animate__fadeIn animate__faster"><a href="/">返回 API 首页</a></p>
-      <span class="tips animate__animated animate__fadeIn animate__faster">NOT_FOUND</span>
-    </div>
-    <script>
-      const pjax = new Pjax({selectors: ['title', '.data-pjax'], cacheBust: false});
-      document.addEventListener('pjax:send', () => document.querySelectorAll('.animate__animated').forEach(e => {
-        e.classList.remove('animate__fadeIn');
-        e.classList.add('animate__fadeOut');
-      }));
-      document.addEventListener('pjax:error', () => document.location.href = event.request.responseURL);
-    </script>
-  </body>
-</html>`);
+      <span class="tips animate__animated animate__fadeIn animate__faster">NOT_FOUND</span>`}));
   }
 };
