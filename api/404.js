c2ccd82006b492298c3cc259dc5293481a811d2f
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-02-09 18:14:26 +0800

    Update 404.js

--- api/404.js
+++ api/404.js
@@ -5,8 +5,8 @@ module.exports = (req, res) => {
   } else {
     res.status(404).send(HTML({title: 'API 不存在', data: `
       <span class="face animate__animated animate__fadeIn animate__faster">:(</span>
-      <p class="content animate__animated animate__fadeIn animate__faster">您访问的 API 不存在，请到<a href="/">首页</a>查看目前可用的 API 列表</p>
-      <p class="home animate__animated animate__fadeIn animate__faster"><a href="/">返回 API 首页</a></p>
+      <p class="content animate__animated animate__fadeIn animate__faster">您访问的 API 不存在，请到<a href="/api/">首页</a>查看目前可用的 API 列表</p>
+      <p class="home animate__animated animate__fadeIn animate__faster"><a href="/api/">返回 API 首页</a></p>
       <span class="tips animate__animated animate__fadeIn animate__faster">NOT_FOUND</span>`}));
   }
 };
