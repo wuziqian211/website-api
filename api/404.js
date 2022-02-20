6d66fe76c2b439da9b5b7789a1aba4e279b363f6
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-02-20 19:30:55 +0800

    Update 404.js

--- api/404.js
+++ api/404.js
@@ -3,20 +3,17 @@ module.exports = (req, res) => res.status(404).send(`<!DOCTYPE html>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#0078B7" media="(prefers-color-scheme: light)" />
-    <meta name="theme-color" content="#000064" media="(prefers-color-scheme: dark)" />
+    <meta name="theme-color" content="#EEE" media="(prefers-color-scheme: light)" />
+    <meta name="theme-color" content="#333" media="(prefers-color-scheme: dark)" />
     <title>页面不存在 | wuziqian211's Blog</title>
     <link rel="stylesheet" href="${process.env.URL}/assets/style.css" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="${process.env.URL}/assets/animate.min.css" />
   </head>
   <body>
-    <!-- Reference code: status.fastgit.org -->
-    <div>
-      <span class="face animate__animated animate__fadeIn animate__faster">:(</span>
+    <header><div class="header"><a class="noul" href="/api/">wuziqian211's Blog API</a></div></header>
+    <div class="main">
       <p class="content animate__animated animate__fadeIn animate__faster">您似乎输入错了网址！您是否想要访问 <a href="https://wuziqian211.top${req.url}">https://wuziqian211.top${req.url}</a>？</p>
-      <p class="home animate__animated animate__fadeIn animate__faster"><a href="https://wuziqian211.top/">返回网站首页</a></p>
-      <span class="tips animate__animated animate__fadeIn animate__faster">NOT_FOUND</span>
     </div>
   </body>
 </html>`);
