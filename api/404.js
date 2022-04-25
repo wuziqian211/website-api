723a1705c4ec3723573365267993d826d86cfea8
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-04-26 00:16:32 +0800

    Update 404.js

--- api/404.js
+++ api/404.js
@@ -3,8 +3,8 @@ module.exports = (req, res) => res.status(404).send(`<!DOCTYPE html>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#EEE" media="(prefers-color-scheme: light)" />
-    <meta name="theme-color" content="#333" media="(prefers-color-scheme: dark)" />
+    <meta name="theme-color" content="#FFF" media="(prefers-color-scheme: light)" />
+    <meta name="theme-color" content="#222" media="(prefers-color-scheme: dark)" />
     <title>页面不存在 | wuziqian211's Blog</title>
     <link rel="stylesheet" href="${process.env.URL}/assets/style.css" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
