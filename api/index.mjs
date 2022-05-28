39515b66e99b2abdc89ff9c3520d16d82f3499df
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-05-28 14:00:07 +0800

    Update index.mjs

--- api/index.mjs
+++ api/index.mjs
@@ -5,7 +5,7 @@ export default (req, res) => {
   try {
     if (utils.getAccept(req) === 1) {
       res.status(200).send(utils.renderHTML({startTime, title: '欢迎来到 API 页面', body: `
-      欢迎您来到 <a href="https://wuziqian211.top/">wuziqian211's Blog</a> 的 API 页面！<br />
+      欢迎您来到 <a target="_blank" href="https://wuziqian211.top/">wuziqian211's Blog</a> 的 API 页面！<br />
       这些 API 主要为 wuziqian211's Blog 的一些功能提供服务。<br />
       <br />
       其中，下面这些 API 是公开的，任何合法网站和程序都能调用：<br />
