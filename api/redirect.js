f7748a6a1faa386932ba2a9ee2fa27c6aed367b8
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-06-04 18:11:50 +0800

    update

---
+++ api/redirect.js
@@ -0,0 +1,4 @@
+export default (req, res) => {
+  const url = req.headers.host.replace(/^(.+\.)?w211.top$/, '$1wuziqian211.top') + req.url;
+  res.status(308).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({code: 308, data: {url}});
+}
