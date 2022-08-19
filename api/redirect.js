ae8bb6d57a211e62480c0899319373a80c60b751
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-08-20 02:06:04 +0800

    update

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default (req, res) => {
   if (/^(?:.+\.)?w211.top$/.test(req.headers.host)) {
     const url = `https://${req.headers.host.replace(/^(.+\.)?w211.top$/, '$1wuziqian211.top')}${req.url}`;
-    res.status(308).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({code: 308, data: {url}});
+    res.status(308).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
   } else {
     res.status(403).send(`<html>
 <head><title>403 Forbidden</title></head>
