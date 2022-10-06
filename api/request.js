27348072b814fdbdd00f8023b1615b9a4c214538
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-10-06 23:55:56 +0800

    Update request.js

--- api/request.js
+++ api/request.js
@@ -2,6 +2,7 @@ import fetch from 'node-fetch';
 export default async (req, res) => {
   let { headers } = req;
   delete headers.host;
+  delete headers.forwarded;
   for (const name in headers) {
     if (name.startsWith('x-')) delete headers[name];
   }
@@ -13,8 +14,22 @@ export default async (req, res) => {
   for (const name in req.query) {
     if (name !== 'url') headers[name] = req.query[name];
   }
-  console.log(req.query, headers);
+  let body;
+  if (req.method === 'POST') {
+    if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+      body = new URLSearchParams();
+      for (const name in req.body) {
+        body.append(name, req.body[name]);
+      }
+    } else {
+      { body } = req;
+    }
+  }
+  console.log(req.query, headers, body);
   const resp = await fetch(req.query.url, { method: req.method, headers });
-  if (resp.status === 0) res.status(404).send('Error Not Found');
-  res.status(resp.status).setHeader('Content-Type', resp.headers.get('Content-Type').replace(/text\/html/, 'text/plain')).send(Buffer.from(await resp.arrayBuffer()));
+  if (resp.status === 0) {
+    res.status(500).send('Connection Error');
+  } else {
+    res.status(resp.status).setHeader('Content-Type', resp.headers.get('Content-Type').replace(/text\/html/, 'text/plain')).send(Buffer.from(await resp.arrayBuffer()));
+  }
 };
