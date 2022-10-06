924a17b92b41cfe9c710f981d8689cbaf978eb46
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-10-06 20:13:03 +0800

    Revert "Update request.js"
    This reverts commit bdf736884b3f961a83b76a8cb01859c13109debf.

--- api/request.js
+++ api/request.js
@@ -5,6 +5,8 @@ const handler = async (req, res) => {
   for (const name in headers) {
     if (name.startsWith('x-')) delete headers[name];
   }
+  const u = new URL(req.query.url);
+  headers.origin = u.origin;
   let t = new URL(req.headers.referer || req.query.url);
   t.host = u.host;
   headers.referer = t.href;
@@ -12,8 +14,6 @@ const handler = async (req, res) => {
     await handler({ headers: req.headers, method: req.method, query: { url: t.origin + req.url }, url: '/api/request' }, res);
     return;
   }
-  const u = new URL(req.query.url);
-  headers.origin = u.origin;
   const resp = await fetch(req.query.url, { method: req.method, headers });
   if (resp.status === 0) res.status(404).send('Error Not Found');
   res.status(resp.status).setHeader('Content-Type', resp.headers.get('Content-Type')).send(Buffer.from(await resp.arrayBuffer()));
