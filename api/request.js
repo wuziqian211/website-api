27bfb0482e8ab834b6552b554eedf359207b5b78
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-10-06 19:36:51 +0800

    test

---
+++ api/request.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+export default async (req, res) => {
+  let { headers } = req;
+  const u = new URL(req.query.url);
+  headers.origin = u.origin;
+  let t = new URL(req.headers.referer);
+  t.host = u.host;
+  headers.referer = t.href;
+  const resp = await fetch(req.query.url, { method: req.method, headers });
+  if (resp.status === 0) res.status(404).send('Error Not Found');
+  res.status(resp.status).setHeader('Content-Type', resp.headers.get('Content-Type')).send(Buffer.from(await resp.arrayBuffer()));
+};
