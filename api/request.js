f8d6a24948dd1e9bd584f74636094b46c7cb75b2
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-10-06 19:56:15 +0800

    update

--- api/request.js
+++ api/request.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-export default async (req, res) => {
+const handler = async (req, res) => {
   let { headers } = req;
   delete headers.host;
   for (const name in headers) {
@@ -10,7 +10,12 @@ export default async (req, res) => {
   let t = new URL(req.headers.referer || req.query.url);
   t.host = u.host;
   headers.referer = t.href;
+  if (req.url !== '/api/request') {
+    await handler({ headers: req.headers, method: req.method, query: { url: t.origin + req.url } });
+    return;
+  }
   const resp = await fetch(req.query.url, { method: req.method, headers });
   if (resp.status === 0) res.status(404).send('Error Not Found');
   res.status(resp.status).setHeader('Content-Type', resp.headers.get('Content-Type')).send(Buffer.from(await resp.arrayBuffer()));
 };
+export default handler;
