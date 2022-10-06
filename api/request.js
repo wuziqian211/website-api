dea3480dccc151ed15c2aab55881141a09f40f23
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-10-06 20:16:20 +0800

    Update request.js

--- api/request.js
+++ api/request.js
@@ -12,5 +12,5 @@ export default async (req, res) => {
   headers.referer = t.href;
   const resp = await fetch(req.query.url, { method: req.method, headers });
   if (resp.status === 0) res.status(404).send('Error Not Found');
-  res.status(resp.status).setHeader('Content-Type', resp.headers.get('Content-Type')).send(Buffer.from(await resp.arrayBuffer()));
+  res.status(resp.status).setHeader('Content-Type', resp.headers.get('Content-Type').replace(/text\/html/, 'text/plain')).send(Buffer.from(await resp.arrayBuffer()));
 };
