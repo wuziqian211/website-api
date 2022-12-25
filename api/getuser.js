893b2824fe0caa7557f8bd7d574f361b151a418a
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-12-25 17:10:34 +0800

    Update getuser.js

--- api/getuser.js
+++ api/getuser.js
@@ -31,7 +31,7 @@ export default async (req, res) => {
                     <a target="_blank" rel="noopener external nofollow noreferrer" href="https://space.bilibili.com/${req.query.mid}">
                       <img class="face" alt title="${utils.encodeHTML(ujson.data.name)}" src="${utils.toHTTPS(ujson.data.face)}" referrerpolicy="no-referrer" />
                       ${ujson.data.pendant?.pid ? `<img class="face-frame" alt title="${utils.encodeHTML(ujson.data.pendant.name)}" src="${utils.toHTTPS(ujson.data.pendant.image_enhance || ujson.data.pendant.image)}" referrerpolicy="no-referrer" />` : ''}
-                      ${ujson.data.face_nft ? '<img class="face-icon${[0, 1].includes(ujson.data.official.type) || ujson.data.vip.status ? ' second' : ''}" alt title="数字藏品" src="/assets/nft-label.gif" />' : ''}
+                      ${ujson.data.face_nft ? `<img class="face-icon${[0, 1].includes(ujson.data.official.type) || ujson.data.vip.status ? ' second' : ''}" alt title="数字藏品" src="/assets/nft-label.gif" />` : ''}
                       ${ujson.data.official.type === 0 ? '<img class="face-icon" alt title="UP 主认证" src="/assets/personal.svg" />' : ujson.data.official.type === 1 ? '<img class="face-icon" alt title="机构认证" src="/assets/business.svg" />' : ujson.data.vip.status ? '<img class="face-icon" alt title="大会员" src="/assets/big-vip.svg" />' : ''}
                     </a>
                   </div>
@@ -80,7 +80,6 @@ export default async (req, res) => {
                     <span class="description">${utils.encodeHTML(cjson.card.sign)}</span>
                   </div>
                 </div>
-                ${[0, 1].includes(cjson.card.official_verify.type) ? `<img class="official-icon" alt title="${cjson.card.official_verify.type === 0 ? 'UP 主认证" src="/assets/personal.svg" /> <strong style="color: #ffc62e;">bilibili UP 主' : '机构认证" src="/assets/business.svg" /> <strong style="color: #4ac7ff;">bilibili 机构'}认证：</strong>${utils.encodeHTML(cjson.card.official_verify.desc)}<br />` : ''}
                 <strong>生日：</strong>${cjson.card.birthday ? utils.encodeHTML(cjson.card.birthday) : '保密'}<br />
                 <strong>注册时间：</strong>${utils.getDate(cjson.card.regtime)}<br />
                 <strong>关注数：</strong>${utils.getNumber(cjson.card.attention)}<br />
