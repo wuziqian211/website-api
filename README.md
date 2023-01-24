8eaad3068811392500551539f51bf0f1b9ff6cc7
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2023-01-25 03:25:09 +0800

    update

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# wuziqian211's Blog 短域名跳转
+# wuziqian211's Blog 域名跳转
 
 ## 功能
 
-当用户访问 “wuziqian211.top” 的短域名 “w211.top” 时，会调用本分支中的 “api/redirect.js” API，以重定向到 “wuziqian211.top”。
+当用户试图以 “wuziqian211.top” 下的未分配到其他项目的子域名访问网站，或者以 “w211.top” 或其子域名访问网站时，会调用本分支中的 “api/redirect.js” API，这个 API 可提示用户可能输入错了网址，或者重定向到对应于 “wuziqian211.top” 的网址。
 
 对本存储库中大部分文件及文件夹的说明：
 
 | 文件或文件夹 | 说明 |
 | ------------ | ---- |
 | api/ | 本文件夹包含所有 API，在网站上访问里面的文件会调用对应 API |
-| api/redirect.js | 访问 “*.w211.top” 域名的任何文件都会调用这个 API |
+| api/redirect.js | 主要的 API |
 | LICENSE | MIT 许可证 |
 | package.json, package-lock.json | 供 Node.js 与 npm 使用 |
 | vercel.json | Vercel（API 服务商）的配置文件 |
--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default (req, res) => {
   if (/^(?:.+\.)?wuziqian211.top$/.test(req.headers.host)) {
     let expectedURL;
-    if (/^\/archive\/202|^\/wik|^\/abo/.test(req.url)) {
+    if (/^\/arc|^\/wik|^\/abo|^\/fri/.test(req.url)) {
       expectedURL = `https://wuziqian211.top${req.url}`;
     } else if (/^\/api|^\/get/.test(req.url)) {
       expectedURL = `https://api.wuziqian211.top${req.url}`;
@@ -20,7 +20,7 @@ export default (req, res) => {
         <body>
           <header>
             <div class="header">
-              <a class="no-underline" href="https://wuziqian211.top/">wuziqian211's Blog</a> <span class="description">一个简单的 Blog</span>
+              <a href="https://wuziqian211.top/">wuziqian211's Blog</a> <span class="description">一个简单的 Blog</span>
             </div>
           </header>
           <main>
@@ -31,7 +31,7 @@ export default (req, res) => {
             © 2021 – 2023 wuziqian211
           </footer>
         </body>
-      </html>`);
+      </html>`).replace(/(?: |\n)+/gm, ' ').trim();
   } else if (/^(?:.+\.)?w211.top$/.test(req.headers.host)) {
     const url = `https://${req.headers.host.replace(/^(.+\.)?w211.top$/, '$1wuziqian211.top')}${req.url}`;
     res.status(308).setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate').setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
