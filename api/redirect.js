243616c6991e9c5977790ce9a279851d6fdc2684
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2023-01-25 03:31:45 +0800

    fix

--- api/redirect.js
+++ api/redirect.js
@@ -31,7 +31,7 @@ export default (req, res) => {
             © 2021 – 2023 wuziqian211
           </footer>
         </body>
-      </html>`).replace(/(?: |\n)+/gm, ' ').trim();
+      </html>`.replace(/(?: |\n)+/gm, ' ').trim());
   } else if (/^(?:.+\.)?w211.top$/.test(req.headers.host)) {
     const url = `https://${req.headers.host.replace(/^(.+\.)?w211.top$/, '$1wuziqian211.top')}${req.url}`;
     res.status(308).setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate').setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
