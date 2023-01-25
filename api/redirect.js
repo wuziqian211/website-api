21c2dc9c700eec1a52c7e7248690e8699f909ba5
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2023-01-25 15:57:45 +0800

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -34,9 +34,12 @@ export default (req, res) => {
       </html>`.replace(/(?: |\n)+/gm, ' ').trim());
   } else if (/^(?:.+\.)?w211.top$/.test(req.headers.host)) {
     const url = `https://${req.headers.host.replace(/^(.+\.)?w211.top$/, '$1wuziqian211.top')}${req.url}`;
-    res.status(308).setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate').setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
+    res.status(307).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
+  } else if (/^(?:.+\.)?yumeharu.top$/.test(req.headers.host)) {
+    const url = `https://${req.headers.host.replace(/^(.+\.)?yumeharu.top$/, '$1wuziqian211.top')}${req.url}`;
+    res.status(307).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
   } else {
     const url = 'https://wuziqian211.top/';
-    res.status(308).setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate').setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
+    res.status(307).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
   }
 };
