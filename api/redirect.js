fdff152b7886ab3b8a89e516a9d7de14ea0befed
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2023-01-25 16:35:38 +0800

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,10 +1,10 @@
 export default (req, res) => {
-  if (/^(?:.+\.)?wuziqian211.top$/.test(req.headers.host)) {
+  if (/^(?:.+\.)?yumeharu.top$/.test(req.headers.host)) {
     let expectedURL;
     if (/^\/arc|^\/wik|^\/abo|^\/fri/.test(req.url)) {
       expectedURL = `https://wuziqian211.top${req.url}`;
     } else if (/^\/api|^\/get/.test(req.url)) {
-      expectedURL = `https://api.wuziqian211.top${req.url}`;
+      expectedURL = `https://api.yumeharu.top${req.url}`;
     }
     res.status(404).setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate').send(`<!DOCTYPE html>
       <html lang="zh-CN">
@@ -33,11 +33,11 @@ export default (req, res) => {
         </body>
       </html>`.replace(/(?: |\n)+/gm, ' ').trim());
   } else if (/^(?:.+\.)?w211.top$/.test(req.headers.host)) {
-    const url = `https://${req.headers.host.replace(/^(.+\.)?w211.top$/, '$1wuziqian211.top')}${req.url}`;
-    res.status(307).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
-  } else if (/^(?:.+\.)?yumeharu.top$/.test(req.headers.host)) {
-    const url = `https://${req.headers.host.replace(/^(.+\.)?yumeharu.top$/, '$1wuziqian211.top')}${req.url}`;
-    res.status(307).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
+    const url = `https://${req.headers.host.replace(/^(.+\.)?w211.top$/, '$1yumeharu.top')}${req.url}`;
+    res.status(308).setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate').setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
+  } else if (/^(?:.+\.)?wuziqian211.top$/.test(req.headers.host)) {
+    const url = `https://${req.headers.host.replace(/^(.+\.)?wuziqian211.top$/, '$1yumeharu.top')}${req.url}`;
+    res.status(308).setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate').setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
   } else {
     const url = 'https://wuziqian211.top/';
     res.status(307).setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
