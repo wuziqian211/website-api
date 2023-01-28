eb556d7b5348d7e521425b0e71196b44555f67f2
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2023-01-29 01:28:15 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -6,6 +6,29 @@ const replacePage = text => {
   const html = new DOMParser().parseFromString(text, 'text/html');
   ['title', 'style.extra', 'div.header span.description', 'main', 'span.time-taken'].forEach(s => document.querySelector(s).innerHTML = html.querySelector(s).innerHTML);
 };
+const load = (url, event) => {
+  if (isLoadAvailable(url)) {
+    event.preventDefault();
+    loadPage(url);
+  }
+};
+const bindLoad = () => {
+  document.querySelectorAll('a').forEach(a => {
+    a.onclick = event => load(a.href, event);
+    a.onkeyup = event => event.code === 'Enter' && load(a.href, event);
+  });
+  document.querySelectorAll('form').forEach(form => {
+    form.onsubmit = event => {
+      let params = new URLSearchParams();
+      for (const e of form.elements) {
+        if (e.tagName.toLowerCase() === 'input' && e.type !== 'submit') {
+          params.append(e.name, e.value);
+        }
+      }
+      load(`${form.getAttribute('action') || ''}?${params.toString()}`, event);
+    };
+  });
+};
 const loadPage = async url => {
   document.querySelector('main').classList.add('loading');
   document.activeElement?.blur();
@@ -18,47 +41,20 @@ const loadPage = async url => {
     const text = await resp.text();
     history.pushState({ text }, '', resp.url);
     replacePage(text);
-    applyLoad();
+    bindLoad();
     document.querySelector('main').classList.remove('loading');
   } catch (e) {
     console.error(e);
     document.location.href = url;
   }
 };
-history.replaceState({ text: document.documentElement.outerHTML }, '');
 window.onpopstate = event => {
   document.activeElement?.blur();
   replacePage(event.state.text);
-  applyLoad();
+  bindLoad();
 };
-const aLoad = (el, event) => {
-  if (isLoadAvailable(el.href)) {
-    event.preventDefault();
-    loadPage(el.href);
-  }
-};
-const applyLoad = () => {
-  document.querySelectorAll('a').forEach(a => {
-    a.onclick = event => aLoad(a, event);
-    a.onkeyup = event => event.code === 'Enter' && aLoad(a, event);
-  });
-  document.querySelectorAll('form').forEach(form => {
-    form.onsubmit = event => {
-      let params = new URLSearchParams();
-      for (const e of form.elements) {
-        if (e.tagName.toLowerCase() === 'input' && e.type !== 'submit') {
-          params.append(e.name, e.value);
-        }
-      }
-      const url = `${form.getAttribute('action') || ''}?${params.toString()}`;
-      if (isLoadAvailable(url)) {
-        event.preventDefault();
-        loadPage(url);
-      }
-    };
-  });
-};
-applyLoad();
+history.replaceState({ text: document.documentElement.outerHTML }, '');
+bindLoad();
 
 const runningTime = document.querySelector('span.running-time');
 const updateTime = () => {
