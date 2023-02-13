8bfdb4429a0830e2e5e0e687a1213476aa4276ed
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2023-02-14 00:31:13 +0800

    fix

--- api/getvideo.js
+++ api/getvideo.js
@@ -122,7 +122,7 @@ const handler = async (req, res) => {
                   </div>
                   <div>
                     <a class="title" target="_blank" rel="noopener external nofollow noreferrer" href="https://space.bilibili.com/${u.mid}">${utils.encodeHTML(u.name)}</a><br />
-                    ${[0, 1].includes(u.official.type) ? `<img class="official-icon icon-${u.official.type === 0 ? 'personal" alt title="UP 主认证" src="/assets/personal.svg" /> <strong style="color: #ffc62e;">bilibili UP 主' : 'business" alt title="机构认证" src="/assets/business.svg" /> <strong style="color: #4ac7ff;">bilibili 机构'}认证：</strong>${utils.encodeHTML(u.official.title)}<br />` : ''}
+                    ${[0, 1].includes(u.official.type) ? `<img class="official-icon icon-${u.official.type === 0 ? 'personal" alt title="UP 主认证" /> <strong style="color: #ffc62e;">bilibili UP 主' : 'business" alt title="机构认证" /> <strong style="color: #4ac7ff;">bilibili 机构'}认证：</strong>${utils.encodeHTML(u.official.title)}<br />` : ''}
                     <strong>粉丝数：</strong>${utils.getNumber(u.follower)}
                   </div>
                 </div>`).join('')}` : `
