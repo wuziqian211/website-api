7b942ce1c26003b6978a195b11ed3f23d69a618f
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2023-05-02 18:05:12 +0800

    Update getvideo.js

--- api/getvideo.js
+++ api/getvideo.js
@@ -158,7 +158,7 @@ const handler = async (req, res) => {
                   </div>
                 </div>`).join('') : ''}
                 <strong>简介：</strong><br />
-                ${json.data.desc_v2 ? json.data.desc_v2.map(d => d.type === 2 ? `<a target="_blank" rel="noopener external nofollow noreferrer" href="https://space.bilibili.com/${d.biz_id}">@${utils.encodeHTML(d.raw_text)}</a>` : utils.encodeHTML(d.raw_text)).join('') : utils.encodeHTML(json.data.desc)}`;
+                ${json.data.desc_v2 ? json.data.desc_v2.map(d => d.type === 2 ? `<a target="_blank" rel="noopener external nofollow noreferrer" href="https://space.bilibili.com/${d.biz_id}">@${utils.encodeHTML(d.raw_text)} </a>` : utils.encodeHTML(d.raw_text)).join('') : utils.encodeHTML(json.data.desc)}`;
               res.status(200);
               sendHTML({ title: `${utils.encodeHTML(json.data.title)} 的信息`, style: utils.renderExtraStyle(utils.toHTTPS(json.data.pic)), content, vid: req.query.vid });
               break;
