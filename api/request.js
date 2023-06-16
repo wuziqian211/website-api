e13c0545148911a7d8e3b5ff852cc3595baa28d8
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2023-06-16 22:53:15 +0800

    Update request.js

--- api/request.js
+++ api/request.js
@@ -1,21 +1,13 @@
 export default async (req, res) => {
   try { 
     const { headers } = req;
-    delete headers.connection;
-    delete headers.host;
-    delete headers.forwarded;
-    delete headers['if-none-match'];
-    for (const name in headers) {
-      if (name.startsWith('x-') || name.startsWith('cf-') || name.startsWith('access-control-')) delete headers[name];
-    }
+    Object.keys(headers).forEach(name => (['connection', 'host', 'forwarded', 'if-none-match'].includes(name) || name.startsWith('x-') || name.startsWith('cf-') || name.startsWith('access-control-')) && delete headers[name]);
     const requestedUrl = new URL(req.query.url);
     headers.origin = requestedUrl.origin;
     const referrer = new URL(req.headers.referer || req.query.url);
     referrer.host = requestedUrl.host;
     headers.referer = referrer.href;
-    for (const name in req.query) {
-      if (name !== 'url') headers[name] = req.query[name];
-    }
+    Object.keys(req.query).forEach(name => name !== 'url' && headers[name] = req.query[name]);
     let body;
     if (req.method === 'POST' || req.method === 'PUT') {
       switch (req.headers['content-type']) {
@@ -32,10 +24,8 @@ export default async (req, res) => {
     const resp = await fetch(req.query.url, { method: req.method, headers, body });
     res.status(resp.status);
     if (resp.headers.has('Content-Type')) res.setHeader('Content-Type', resp.headers.get('Content-Type').replace(/text\/html/g, 'text/plain'));
-    for (const h of ['Content-Disposition', 'Content-Range']) {
-      if (resp.headers.has(h)) res.setHeader(h, resp.headers.get(h));
-    }
-    for (const [name, value] of resp.headers) res.setHeader('X-Http-' + name, value);
+    ['Content-Disposition', 'Content-Range'].forEach(h => resp.headers.has(h) && res.setHeader(h, resp.headers.get(h)));
+    resp.headers.forEach((name, value) => res.setHeader('X-Http-' + name, value));
     res.send(Buffer.from(await resp.arrayBuffer()));
   } catch (e) {
     res.status(500).send(`Error<br /><pre>${e.stack.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/ (?= )|(?<= ) |^ | $/gm, '&nbsp;').replace(/\n/g, '<br />')}</pre>`);
