e8a531ff8aea86c4b9b5e7477856e46628f978ae
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2024-02-10 23:54:57 +0800

    fix

--- api/redirect.js
+++ api/redirect.js
@@ -34,7 +34,7 @@ export default (req, res) => {
             © 2021 – ${new Date(Date.now() + (new Date().getTimezoneOffset() + 480) * 60000).getFullYear()} wuziqian211
           </footer>
         </body>
-      </html>`.replace(/([ \n]+/g, ' ').trim());
+      </html>`.replace(/[ \n]+/g, ' ').trim());
   } else if (/^(?:.+\.)?w211.top$/.test(req.headers.host)) {
     const url = `https://${req.headers.host.replace(/^(.+\.)?w211.top$/, '$1yumeharu.top')}${req.url}`;
     res.status(308).setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate').setHeader('Location', url).setHeader('Refresh', `0; url=${url}`).json({ code: 308, data: { url } });
