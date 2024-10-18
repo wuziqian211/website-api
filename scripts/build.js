af509e28f17c6e8407d809c92c6216885f4604e2
晨叶梦春 <65224318+wuziqian211@users.noreply.github.com>
2024-10-19 02:05:53 +0800

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -4,6 +4,10 @@ const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, li
 const { SESSDATA, csrf } = process.env;
 
 const ujson = await (await fetch('https://api.bilibili.com/x/web-interface/nav', { headers: { Cookie: `SESSDATA=${SESSDATA}; bili_jct=${csrf}`, Origin: 'https://www.bilibili.com', Referer: 'https://www.bilibili.com/', 'User-Agent': userAgent } })).json();
+if (ujson.code !== 0) {
+  console.error('获取账号登录信息失败，请重新设置 Cookie');
+  process.exit(1);
+}
 const { mid } = ujson.data;
 console.log(`当前登录账号 UID：${mid}`);
 
